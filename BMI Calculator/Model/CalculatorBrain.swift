d8627d189e289166d35e1b737a794add105051ec
Andrey <mands315@gmail.com>
2023-04-15 14:00:29 +0300

    Improve model

--- BMI Calculator/Model/CalculatorBrain.swift
+++ BMI Calculator/Model/CalculatorBrain.swift
@@ -5,17 +5,27 @@
 //  Created by Andrey on 15.04.2023.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor(rgb: 0x6cd0f7))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor(rgb: 0xadde8a))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor(rgb: 0xe46d99))
+        }
+        
+        
     }
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi ?? 0.0)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
     }
 }
